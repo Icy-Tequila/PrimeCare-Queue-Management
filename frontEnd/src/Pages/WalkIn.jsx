747b209d8aef1f8b6e3e444df97194eb565ce7fd
Icy-Tequila <janeilletrixiedeleon@gmail.com>
2025-06-01 19:44:47 +0800

    feat: add individual start screens and set up routing for admin, appointment, walk-in, and lobby modules using react-router-dom

---
+++ frontEnd/src/Pages/WalkIn.jsx
@@ -0,0 +1,27 @@
+import primeCareLogo from "../assets/Images/primeCare-logo.png";
+
+const WalkInPatient = () => {
+  return (
+    <button className="min-h-screen w-full">
+      <div className="w-full min-h-screen flex flex-wrap justify-center items-center">
+        <div className="text-center">
+          <h1 className="lg:text-lg text-sm mb-4">Welcome to</h1>
+          <div className="flex gap-1 items-center justify-center">
+            <img
+              src={primeCareLogo}
+              className="lg:w-7 w-5 lg:h-7 h-5 mt-1"
+            ></img>
+            <h1 className="lg:text-3xl text-xl font-bold text-[#428ce2]">
+              PRIMECARE <span className="text-[#0255a3]">CLINIC</span>
+            </h1>
+          </div>
+        </div>
+      </div>
+      <h1 className="fixed bottom-20 w-full text-[#428ce2] text-center lg:text-lg text-sm">
+        Tap anywhere to start
+      </h1>
+    </button>
+  );
+};
+
+export default WalkInPatient;
--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import AppointmentPatient from './Pages/Appointment.jsx';
+import LobbyScreen from './Pages/Lobby.jsx';
+import WalkInPatient from './Pages/WalkIn.jsx'; 
+import Admin from './Pages/Admin.jsx';
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/lobby",
+    element: <LobbyScreen />,
+  },
+  {
+    path: "/appointment",
+    element: <AppointmentPatient />,
+  },
+  {
+    path: "/walk-in",
+    element: <WalkInPatient />,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
+  // You can add a "not found" route or redirects if you want
+]);
+
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
